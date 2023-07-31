99e2edd93ff4c3c08537800a9de6d1b2235be2dd
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-07-31 20:02:27 +0400

    Add super method and fix linter errors

--- person.rb
+++ person.rb
@@ -2,6 +2,7 @@ require_relative 'nameable'
 
 class Person < Nameable
   def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..200)
     @name = name
     @age = age
