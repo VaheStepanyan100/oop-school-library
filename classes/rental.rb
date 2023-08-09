e05646a2b978e1570701c1a2bf407d75516c1ad4
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-08-09 10:51:14 +0300

    Add to_hash method for rental

--- classes/rental.rb
+++ classes/rental.rb
@@ -1,11 +1,21 @@
+require_relative 'book'
+
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date, book, person)
+  def initialize(date, book, person, person_book_index)
     @date = date
     @person = person
     @book = book
-    person&.add_rental(self)
-    book&.add_rental(self)
+    @person_book_index = person_book_index
+  end
+
+  def to_hash
+    {
+      'date' => @date,
+      'person' => @person.to_hash,
+      'book' => @book.to_hash,
+      'person_book_index' => @person_book_index
+    }
   end
 end
