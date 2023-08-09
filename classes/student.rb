2da473b3c8482ce69edde3d632a7ad22e731233b
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-08-09 10:53:36 +0300

    Add to_hash method for student

--- classes/student.rb
+++ classes/student.rb
@@ -3,13 +3,25 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  def initialize(classroom, age, parent_permission: true, name: 'Unknown', id: nil)
+    id ||= Random.rand(1..200)
+    super(id, age, name: name, parent_permission: parent_permission)
     @classroom = classroom
     classroom&.add_student(self)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def to_hash
+    {
+      'id' => @id,
+      'type' => 'Student',
+      'name' => @name,
+      'age' => @age,
+      'parent_permission' => @parent_permission,
+      'rentals' => @rentals.map { |rental| { 'date' => rental.date } }
+    }
   end
 end
