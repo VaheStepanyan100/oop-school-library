606ecdb679be48d231861b22b786543271341512
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-08-09 13:59:06 +0400

    Update

--- classes/person.rb
+++ classes/person.rb
@@ -1,9 +1,9 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(id, age, name: 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..200)
+    @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
--- classes/teacher.rb
+++ classes/teacher.rb
@@ -1,12 +1,25 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(specialization, age, name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  def initialize(specialization, age, name: 'Unknown', id: nil, parent_permission: true)
+    id ||= Random.rand(1..200)
+    super(id, age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
   def can_use_services?
     true
+  end
+
+  def to_hash
+    {
+      'id' => @id,
+      'type' => self.class.name,
+      'name' => @name,
+      'age' => @age,
+      'specialization' => @specialization,
+      'parent_permission' => @parent_permission,
+      'rentals' => @rentals.map { |rental| { 'date' => rental.date } }
+    }
   end
 end
